5327437588fbda8e2da8fe041ec1d7853a73d06f
Nishantit <73215548+Nishantit@users.noreply.github.com>
2025-03-01 19:40:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingradients_list = st.multiselect(
     'Choose Up to 5 ingradients:' , my_dataframe, max_selections=5
